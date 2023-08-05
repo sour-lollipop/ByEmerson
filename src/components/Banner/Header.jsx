641bde0f19efbc68c43f4f26d45efc618cece2e1
sour-lollipop <130147071+sour-lollipop@users.noreply.github.com>
2023-08-05 17:56:06 +0600

    Update Header.jsx

--- src/components/Banner/Header.jsx
+++ src/components/Banner/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
                 <span><a href="#Introduct" onClick={() => setBar(false)} >О НАС</a></span>
                 <span><a href="#footer" onClick={() => setBar(false)} >КОНТАКТЫ</a></span>
                  <span >
-                <a href="wa.me/77717488201" ><img src={logoImg1} alt='WhatsApp' style={{marginRight: '10px'}}/></a>
+                <a href="https://wa.me/77717488201" ><img src={logoImg1} alt='WhatsApp' style={{marginRight: '10px'}}/></a>
                 <a href="#"><img src={logoImg2} alt='Telegram' style={{marginRight: '10px'}}/></a>
                 <a href="https://www.instagram.com/emerson_brand_/"><img src={logoImg3} alt='Instagram' /></a>
             </span>
